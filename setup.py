effb3f379bd3bf96cf275d7df6f91cf6c6ecaf13
KwintenTorfs <kwinten.torfs@kuleuven.be>
2024-01-03 13:48:02 +0100

    Basics

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='imageQualityCT',
+      version='0.0.1',
+      description='Image Quality in CT images',
+      author='Kwinten Torfs',
+      author_email='kwinten.torfs@kuleuven.be',
+      url='https://github.com/KwintenTorfs/ImageQualityCT',
+      packages=['imageQualityCT'],
+      license='KUL',
+      )
